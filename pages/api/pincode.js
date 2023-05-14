45af95500bd35b70094595d4420dd7641f85f0a0
Aryan Kumar <aryanak9163@gmail.com>
2023-05-15 02:40:10 +0530

    adding pincode chekout

---
+++ pages/api/pincode.js
@@ -0,0 +1,3 @@
+export default function handler(req, res) {
+    res.status(200).json([843301, 800013])
+}
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Slug = () => {
+    const [pin, setPin] = useState()
+    const [service, setService] = useState()
+    const checkServiceability = async () => {
+        let pins = await fetch('http://localhost:3000/api/pincode')
+        let pinsjson = await pins.json()
+        if (pinsjson.includes(parseInt(pin))) {
+            setService(true)
+
+        }
+        else {
+            setService(false)
+        }
+    }
+    const onChangePin = (e) => {
+        setPin(e.target.value)
+    }
     return (
         <>
             <section className="text-gray-600 body-font overflow-hidden">
@@ -8,8 +24,8 @@ const Slug = () => {
                     <div className="lg:w-4/5 mx-auto flex flex-wrap">
                         <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto px-24 object-cover object-top rounded" src="https://m.media-amazon.com/images/I/71eUwDk8z+L._UX466_.jpg" />
                         <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                            <h2 className="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2>
-                            <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">The Catcher in the Rye</h1>
+                            <h2 className="text-sm title-font text-gray-500 tracking-widest">CODESWEAR</h2>
+                            <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">Wear the code (Xl/Blue)</h1>
                             <div className="flex mb-4">
                                 <span className="flex items-center">
                                     <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 text-pink-500" viewBox="0 0 24 24">
@@ -73,14 +89,21 @@ const Slug = () => {
                                 </div>
                             </div>
                             <div className="flex">
-                                <span className="title-font font-medium text-2xl text-gray-900">₹58.00</span>
-                                <button className="flex ml-14 text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded">Add to cart</button>
+                                <span className="title-font font-medium text-2xl text-gray-900">₹458.00</span>
+                                <button className="flex ml-8 text-white bg-pink-500 border-0 py-2 px-2 text-sm md:px-6 focus:outline-none hover:bg-pink-600 rounded">Add to cart</button>
+                                <button className="flex ml-4 text-white bg-pink-500 border-0 py-2 px-2 text-sm md:px-6 focus:outline-none hover:bg-pink-600 rounded">Buy Now</button>
                                 <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                                     <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
                                         <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
                                     </svg>
                                 </button>
                             </div>
+                            <div className="pin mt-6 flex space-x-2 text-sm">
+                                <input onChange={onChangePin} type="text" className='px-2 border-2 border-gray-400 rounded-md' placeholder='Enter your PINCODE here' />
+                                <button onClick={checkServiceability} className="text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded">Check</button>
+                            </div>
+                            {(!service && service != null) && <div className="text-red-700 text-sm mt-3">Sorry! We do not deliver to this pincode yet.</div>}
+                            {(service && service != null) && <div className="text-green-700 text-sm mt-3">Yey! This Pincode is serviceable</div>}
                         </div>
                     </div>
                 </div>
