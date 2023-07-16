24b40e92a6740feca717849626680547664930f1
aryan-ji <aryanak9163@gmail.com>
2023-07-16 23:58:18 +0530

    fixed cart

--- pages/cart.js
+++ pages/cart.js
@@ -7,127 +7,94 @@ import { Button } from '@nextui-org/react'
 import { ScrollTop } from 'primereact/scrolltop'
 import CartItem from '../components/CartItem'
 
-
-const cart = ({ cart, removeFromCart }) => {
+const Cart = ({ cart, removeFromCart }) => {
     const [isEmpty, setIsEmpty] = useState(true)
     const router = useRouter()
+
     useEffect(() => {
         if (Object.keys(cart).length > 0) {
             setIsEmpty(false)
         }
     }, [cart])
+
     return (
         <>
             <div className='container mx-auto min-h-screen overflow-x-hidden'>
                 <h1 className='text-3xl text-center my-4 leading-10'>My Cart</h1>
                 {isEmpty ? emptyCart(router) : cartItem(cart, removeFromCart)}
-
-
             </div>
-
         </>
     )
 }
 
 const cartItem = (cart, removeFromCart) => {
-    const routers = useRouter()
+    const router = useRouter()
+
     return (
         <>
-
             <main className='container relative mx-auto w-full'>
-                {/* <div className='flex flex-row justify-start shadow-lg border border-separate items-baseline md:items-center my-4 ml-0 md:ml-10 w-full md:w-[95%]'>
-                    <div className="img md:h-auto ml-4">
+                <div className='flex flex-row justify-start shadow-lg border border-separate items-baseline md:items-center my-4 ml-0 md:ml-10 w-full md:w-[95%]'>
+                    <div className='img md:h-auto ml-4'>
                         <Image
                             src={CartImg}
-                            alt="cart"
+                            alt='cart'
                             width={300}
                             height={300}
-                        // placeholder='blur'
-                        // blurDataURL='/assets/loading-loading-forever.gif'
-                        // onError={() => {
-                        //     setImg('/assets/not-found.png')
-                        // }}
-                        // fill={true}
                         />
                     </div>
-                    <div className="content flex flex-col mb-32 ml-10 items-start">
-                        <h3 className='font-semibold text-xl !my-4'>Good vibes White Oversized T -Shirt</h3>
+                    <div className='content flex flex-col mb-32 ml-10 items-start'>
+                        <h3 className='font-semibold text-xl !my-4'>Good vibes White Oversized T-Shirt</h3>
                         <div className='!my-4'>
                             Size : <span className='font-semibold'>M</span>
                         </div>
                         <div className='quantity border p-2 !my-4'>
                             <button>-</button>
-                            <span className='!mx-10'>
-                                1
-                            </span>
+                            <span className='!mx-10'>1</span>
                             <button>+</button>
                         </div>
                         <div className='flex flex-row justify-between items-center w-full my-4'>
                             <div className='flex flex-col justify-center items-start'>
                                 <h3 className='font-semibold text-xl'>₹ 499</h3>
                             </div>
-                            {/* <button className='flex justify-center items-center text-center border rounded-2xl p-4 text-white !bg-black' type="button" onClick={() => {
-                            router.push('/checkout')
-                        }}>Buy Now</button> */}
-                {/* </div>
-                        <div className='button text-gray-500 -ml-4'>
-                            <button
-                                className='flex justify-center items-center text-center p-4 hover:text-black'
-                            >
-                                Remove
-                            </button>
-
+                            <div className='button text-gray-500 -ml-4'>
+                                <button className='flex justify-center items-center text-center p-4 hover:text-black'>
+                                    Remove
+                                </button>
+                            </div>
                         </div>
-
-                    </div> */}
-                {/* </div>  */}
-
-                <CartItem
-                    cart={cart}
-                    removeFromCart={removeFromCart}
-                />
-
-
-
-            </main >
-            <div className=" fixed bottom-0 p-4 left-0 bg-slate-50 grid grid-rows-3 w-full">
-                <div className="shipping grid grid-cols-2 place-content-center gap-10 py-4">
-                    <div className='ml-5 md:ml-10'>
-                        Shippng Charge :
                     </div>
+                </div>
+                <CartItem cart={cart} removeFromCart={removeFromCart} />
+            </main>
+
+            <div className='fixed bottom-0 p-4 left-0 bg-slate-50 grid grid-rows-3 w-full'>
+                <div className='shipping grid grid-cols-2 place-content-center gap-10 py-4'>
+                    <div className='ml-5 md:ml-10'>Shipping Charge:</div>
                     <div className='font-semibold text-end mr-10'>₹ 50</div>
-
                 </div>
-                <div className="subtotal grid grid-cols-2 place-content-center py-4 font-bold">
-                    <div className='ml-5 md:ml-10'>
-                        Subtotal :
-                    </div>
+                <div className='subtotal grid grid-cols-2 place-content-center py-4 font-bold'>
+                    <div className='ml-5 md:ml-10'>Subtotal:</div>
                     <div className='font-semibold text-end mr-10'>₹ 4450</div>
-
                 </div>
-
-
             </div>
-            <div className="buttons fixed bottom-0 left-0 grid grid-cols-2 place-content-between py-4 bg-black text-white w-full font-black">
-                <div className='ml-5 md:ml-10 my-2'>
-                    ₹ 4500
-                </div>
+
+            <div className='buttons fixed bottom-0 left-0 grid grid-cols-2 place-content-between py-4 bg-black text-white w-full font-black'>
+                <div className='ml-5 md:ml-10 my-2'>₹ 4500</div>
                 <div className='font-semibold item-end mr-10 relative -right-10 md:-right-[40rem]'>
                     <Button
                         auto
                         flat
                         ripple={false}
-                        css={{ color: 'Black', backgroundColor: 'White' }}
+                        css={{ color: 'black', backgroundColor: 'white' }}
                         onClick={() => {
-                            routers.push('/checkout')
+                            router.push('/checkout')
                         }}
                     >
-                        Procceed
+                        Proceed
                     </Button>
                 </div>
-            </div >
+            </div>
         </>
-
     )
 }
 
@@ -137,15 +104,9 @@ const emptyCart = (router) => {
             <div className='flex flex-col justify-center items-center my-16'>
                 <Image
                     src={emptyImg}
-                    alt="empty"
+                    alt='empty'
                     width={200}
                     height={200}
-                    placeholder='blur'
-                    blurDataURL='/assets/loading-loading-forever.gif'
-                    onError={() => {
-                        setImg('/assets/not-found.png')
-                    }}
-                // fill={true}
                 />
             </div>
             <h5 className='mx-auto text-center font-black text-2xl'>Your Cart is empty</h5>
@@ -157,4 +118,4 @@ const emptyCart = (router) => {
     )
 }
 
-export default cart
+export default Cart
