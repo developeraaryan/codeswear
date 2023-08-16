973a5d7e25053cc7465456373b524429442013eb
aryan-ji <aryanak9163@gmail.com>
2023-08-16 05:41:42 +0530

    modified userexists.js api

--- pages/api/userexists.js
+++ pages/api/userexists.js
@@ -1,7 +1,6 @@
 import User from "../../Models/User"
 import connectDb from "../../middleware/mongoose"
 const handler = async (req, res) => {
-    try {
         if (req.method === "POST") {
             const { phone } = req.body;
             const userExists = await User.findOne({ phone });
@@ -13,9 +12,6 @@ const handler = async (req, res) => {
         } else {
             res.status(400).json({ error: "This method is not allowed" });
         }
-    } catch (error) {
-        res.status(500).json({ error: "An error occurred" });
-    }
 }
 
 export default connectDb(handler)
