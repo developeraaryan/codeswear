49814528099eb420a0cb71b28decdb2c795b6e53
aryan-ji <aryanak9163@gmail.com>
2023-08-16 07:21:28 +0530

    fixing getrole.js

--- pages/api/getrole.js
+++ pages/api/getrole.js
@@ -3,20 +3,16 @@ import User from "../../Models/User"; // Make sure the path is correct and case-
 
 const handler = async (req, res) => {
     if (req.method === "POST") { // Use strict equality (===) for comparison
-        try {
             let phone = req.body.phone; // Assuming phone is a property in the request body
             phone = Number.parseInt(phone);
 
             const user = await User.findOne({ phone });
 
             if (user) { // Check if user exists before accessing properties
-                res.status(200).json({ success: true, role: user.role }); // Changed success value to true
+                res.status(200).json({ success: true, role: user?.role }); // Changed success value to true
             } else {
-                res.status(404).json({ success: false, message: "User not found" });
+                res.status(200).json({ success: false, message: "User not found" });
             }
-        } catch (error) {
-            res.status(500).json({ success: false, message: "An error occurred" });
-        }
     } else {
         res.status(405).json({ success: false, message: "Method not allowed" });
     }
