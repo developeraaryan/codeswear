5f1c5224db6bdaae169b3b598ad575074a8d00f0
Aryan Kumar <Aryanak9163@gmail.com>
2024-02-16 01:36:39 +0530

    fixed error

--- components/ScrollToTop.js
+++ components/ScrollToTop.js
@@ -2,11 +2,7 @@
 "use client";
 import { useEffect } from "react";
 
-<<<<<<< HEAD
-export default function scrollToTop() {
-=======
 export default function ScrollToTop() {
->>>>>>> 427dba7c5c1b32db8302a17b65e93c824c1a1efe
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
